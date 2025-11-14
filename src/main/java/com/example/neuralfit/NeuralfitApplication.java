0827a0f5fd51464e8eca1731c809bf13d68f6e9c
KTH <pni2396@gmail.com>
2025-11-15 00:41:19 +0900

    setting: project init

---
+++ src/main/java/com/example/neuralfit/NeuralfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.neuralfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuralfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neuralfit/NeuralfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neuralfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
