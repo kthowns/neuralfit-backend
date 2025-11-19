63b2a9ef754b09dd69e8ae1fd3069e92b4a66e9e
KTH <pni2396@gmail.com>
2025-11-20 01:29:25 +0900

    fix: UserRole 관련 코드 수정

--- src/main/java/com/example/neuralfit/common/dto/AppUserInfoDto.java
+++ src/main/java/com/example/neuralfit/common/dto/AppUserInfoDto.java
@@ -5,7 +5,6 @@ import com.example.neuralfit.common.entity.AppUser;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
